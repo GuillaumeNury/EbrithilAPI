87daa23e5e1d6341e38a33eb6aa1cb762c5ab980
Guillaume Nury <nury.guillaume@gmail.com>
2016-02-15 23:02:12 +0100

    ADD > Add Start scripts

---
+++ start.bat
@@ -0,0 +1,1 @@
+java -cp "target/restx/classes;target/dependency/*" -Drestx.app.package=ebrithilapi -Drestx.mode=prod %VM_OPTIONS%  ebrithilapi.AppServer
---
+++ start.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+
+java -cp "target/restx/classes:target/dependency/*" -Drestx.app.package=ebrithilapi -Drestx.mode=prod $VM_OPTIONS  ebrithilapi.AppServer
